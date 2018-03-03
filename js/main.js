3935beb1e831dab5f8b40d3a2a479d8321a70e9c
dboafor <delademboafor@gmail.com>
2018-03-03 14:39:14 +0000

    Prisal

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$('.slider').slick({
+  infinite: true,
+  slideToShow:1,
+  slideToScroll:1
+});
+
+// $(document).ready(function(){
+//   $('.carousel').slick({
+//   slidesToShow: 3,
+//   dots:true,
+//   centerMode: true,
+//   });
+// });
+
+$('.carousel').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
